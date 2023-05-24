725f204891a841670e9277896eb04d29c39dbc0d
Madison <thefourthb@gmail.com>
2023-05-24 16:28:39 -0700

    new profile pic

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import Portrait from "../../images/avatar.jpg";
+import Portrait from "../../images/Profile.png";
 
 function Home() {
   return (
     <section className="home my-5">
       <div className="title">
         <h2>Hi, I'm Lavinia</h2>
-        <p>Welcome to my Student Portfolio!</p>
+        <p>Welcome to my Portfolio!</p>
         <Link to="portfolio">
           <button>View my Projects</button>
         </Link>
