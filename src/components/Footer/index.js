83d9a09d89d0d31d0b75895da08a18e8f28a9c31
Madison <thefourthb@gmail.com>
2023-05-25 17:18:11 -0700

    new linkedin link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer() {
               <FaGithub />
             </li>
           </a>
-          <a href="https://www.linkedin.com/in/lavinia-b-8b171ba3/" target="blank">
+          <a href="https://www.linkedin.com/in/lavinia-boettcher/" target="blank">
             <li>
               <FaLinkedin />
             </li>
